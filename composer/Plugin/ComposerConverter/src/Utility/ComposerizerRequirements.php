f0bc4c4678e49868c874b0592a74e1c35ee7454e
Paul Mitchum <paul@mile23.com>
2019-10-13 14:18:44 -0700

    Hook in hook_requirements.

---
+++ composer/Plugin/ComposerConverter/src/Utility/ComposerizerRequirements.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Grasmash\ComposerConverter\Utility;
+
+class ComposerizerReqirements implements RequirementsInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function requirements($phase) {
+    
+
+
+  }
+
+}
